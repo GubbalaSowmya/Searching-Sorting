1c6e6a382b13b5bf8f7776092a30537ff4849d52
Sowmya G <sowmyagubbala0681@gmail.com>
2022-09-15 12:27:31 +0530

    Add project files.

---
+++ Searching-Sorting/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    public class sorting
+    {
+        static void Main()
+        {
+
+            string file = "D:\\C#\\Searching-Sorting\\Searching-Sorting\\Details.txt";
+            string[] textLines = File.ReadAllLines(file);
+
+
+            Console.WriteLine("Enter the Search Keyword:");
+            string searchKeyword = Convert.ToString(Console.ReadLine());
+            List<string> results = new List<string>();
+
+            foreach (string line in textLines)
+            {
+                while (line.Contains(searchKeyword))
+                {
+                    results.Add(line);
+                    Console.WriteLine("Keyword is\n" + line);
+                    break;
+
+
+                }
+                if (true)
+                {
+                    Console.WriteLine("Key Not Found");
+                    break;
+
+                }
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
